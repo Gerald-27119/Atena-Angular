3a40f3936f65f1460bbe771473e3a2587f9790ac
Adam Langmesser <s27119@pjwstk.edu.pl>
2024-03-25 20:53:54 +0100

    prace nad question.service.ts

---
+++ src/app/service/question.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, tap, throwError} from "rxjs";
+import {CustomResponse} from "../interface/custom-response";
+import {Topic} from "../enums/topic.enum";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+  private readonly apiUrl = 'nothing';
+
+  constructor(private http: HttpClient) {
+  }
+
+  topics$ = <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/question/list/topics`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  questionsForGivenTopic$ = (topic: Topic) => <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/question/list/${topic}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  question$ = (id: number) => <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/question/${id}`)//1:26
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(`Error ${error} occurred`);
+    return throwError(() => new Error(`An error occurred - Error code: ${error.status}`));
+  }
+}
